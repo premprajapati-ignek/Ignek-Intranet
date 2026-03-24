312117143f0eec4ed28f8ad00f0d355cf7461e92
premprajapati-ignek <prem.prajapati.ignek@gmail.com>
2026-03-24 17:04:31 +0530

    Mapped the employee table to the user_ table

--- modules/employee-web/src/main/java/com/ignek/employee/web/portlet/EmployeeWebPortlet.java
+++ modules/employee-web/src/main/java/com/ignek/employee/web/portlet/EmployeeWebPortlet.java
@@ -7,10 +7,14 @@ import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserConstants;
 import com.liferay.portal.kernel.portlet.PortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import javax.portlet.*;
-
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.lowagie.text.Document;
 import com.lowagie.text.Font;
@@ -19,10 +23,12 @@ import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Component(
 	property = {
@@ -41,54 +47,101 @@ import java.util.List;
 public class EmployeeWebPortlet extends MVCPortlet {
 	private Log log = LogFactoryUtil.getLog(this.getClass().getName());
 
-	@ProcessAction(name = "addUpdateEmployee")
+	@ProcessAction(name = EmployeeWebPortletKeys.ADD_UPDATE_EMPLOYEE)
 	public void addUpdateEmployee(ActionRequest actionRequest, ActionResponse actionResponse){
-		long employeeId = ParamUtil.getLong(actionRequest, "employeeId");
-		String firstName = ParamUtil.getString(actionRequest, "firstName");
-		String lastName = ParamUtil.getString(actionRequest, "lastName");
-		String designation = ParamUtil.getString(actionRequest, "designation");
-		String emailAddress = ParamUtil.getString(actionRequest, "emailAddress");
-		String phoneNumber = ParamUtil.getString(actionRequest, "phoneNumber");
-		String addressLine1 = ParamUtil.getString(actionRequest, "addressLine1");
-		String addressLine2 = ParamUtil.getString(actionRequest, "addressLine2");
-		String city = ParamUtil.getString(actionRequest, "city");
-		String zipCode = ParamUtil.getString(actionRequest, "zipCode");
+		try{
+			long employeeId = ParamUtil.getLong(actionRequest, EmployeeWebPortletKeys.EMPLOYEE_ID);
+			String firstName = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.FIRST_NAME);
+			String lastName = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.LAST_NAME);
+			String designation = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.DESIGNATION);
+			String emailAddress = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.EMAIL_ADDRESS);
+			String phoneNumber = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.PHONE_NUMBER);
+			String addressLine1 = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.ADDRESS_LINE_1);
+			String addressLine2 = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.ADDRESS_LINE_2);
+			String city = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.CITY);
+			String zipCode = ParamUtil.getString(actionRequest, EmployeeWebPortletKeys.ZIP_CODE);
 
-		Employee employee = null;
+			Employee employee = null;
 
-		if (employeeId > 0){
-			try{
-				employee = employeeLocalService.getEmployee(employeeId);
-			} catch (Exception e){
-				log.error(e.getCause(), e);
+			if (employeeId > 0){
+				try{
+					employee = employeeLocalService.getEmployee(employeeId);
+
+					User user = userLocalService.getUser(employee.getUserId());
+					user.setFirstName(firstName);
+					user.setLastName(lastName);
+					user.setEmailAddress(emailAddress);
+					user.setJobTitle(designation);
+					user.setScreenName(firstName+lastName);
+					userLocalService.updateUser(user);
+				} catch (Exception e){
+					log.error(e.getCause(), e);
+				}
+			} else {
+				ServiceContext serviceContext = ServiceContextFactory.getInstance(Employee.class.getName(), actionRequest);
+				long creatorUserId = serviceContext.getUserId();
+				long companyId = serviceContext.getCompanyId();
+				long groupId = serviceContext.getScopeGroupId();
+
+				boolean autoScreenName = true;
+				boolean autoPassword = false;
+				boolean male = true;
+				boolean sendMail = false;
+				String password1 = EmployeeWebPortletKeys.PASSWORD1;
+				String password2 = EmployeeWebPortletKeys.PASSWORD2;
+				String screenName = firstName + lastName;
+				String middleName = EmployeeWebPortletKeys.MIDDLE_NAME;
+				LocalDate birthDate = LocalDate.of(EmployeeWebPortletKeys.BIRTH_YEAR, EmployeeWebPortletKeys.BIRTH_MONTH, EmployeeWebPortletKeys.BIRTH_DAY);
+				int birthYear = birthDate.getYear();
+				int birthMonth = birthDate.getMonthValue();
+				int birthDay = birthDate.getDayOfMonth();
+				int type = UserConstants.TYPE_REGULAR;
+				Locale locale= Locale.getDefault();
+
+				long groupIds[] = new long[10];
+				groupIds[0] = groupId;
+
+				User user = userLocalService.addUser(creatorUserId, companyId, autoPassword, password1, password2,
+						autoScreenName, screenName, emailAddress, locale, firstName, middleName, lastName, -1, -1, male,
+						birthMonth, birthDay, birthYear, designation, type, groupIds, null, null, null, sendMail,
+						serviceContext);
+
+				employeeId = counterLocalService.increment(Employee.class.getName());
+				employee = employeeLocalService.createEmployee(employeeId);
+				employee.setUserId(user.getUserId());
+				employee.setGroupId(groupId);
+				employee.setCompanyId(companyId);
+				employee.setCreateDate(new Date());
 			}
-		} else {
-			employeeId = counterLocalService.increment(Employee.class.getName());
-			employee = employeeLocalService.createEmployee(employeeId);
-		}
 
-		employee.setEmployeeId(employeeId);
-		employee.setFirstName(firstName);
-		employee.setLastName(lastName);
-		employee.setDesignation(designation);
-		employee.setEmailAddress(emailAddress);
-		employee.setPhoneNumber(phoneNumber);
-		employee.setAddressLine1(addressLine1);
-		employee.setAddressLine2(addressLine2);
-		employee.setCity(city);
-		employee.setZipCode(zipCode);
+			employee.setEmployeeId(employeeId);
+			employee.setFirstName(firstName);
+			employee.setLastName(lastName);
+			employee.setDesignation(designation);
+			employee.setEmailAddress(emailAddress);
+			employee.setPhoneNumber(phoneNumber);
+			employee.setAddressLine1(addressLine1);
+			employee.setAddressLine2(addressLine2);
+			employee.setCity(city);
+			employee.setZipCode(zipCode);
+			employee.setModifiedDate(new Date());
 
-		if (employeeId > 0 && employee.isNew()){
-			employeeLocalService.addEmployee(employee);
-		} else {
-			employeeLocalService.updateEmployee(employee);
+			if (employeeId > 0 && employee.isNew()){
+				employeeLocalService.addEmployee(employee);
+			} else {
+				employeeLocalService.updateEmployee(employee);
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		}
 	}
 
-	@ProcessAction(name = "deleteEmployee")
+	@ProcessAction(name = EmployeeWebPortletKeys.DELETE_EMPLOYEE)
 	public void deleteEmployee(ActionRequest actionRequest, ActionResponse actionResponse){
-		long employeeId = ParamUtil.getLong(actionRequest, "employeeId");
+		long employeeId = ParamUtil.getLong(actionRequest, EmployeeWebPortletKeys.EMPLOYEE_ID);
 		try{
+			Employee employee = employeeLocalService.getEmployee(employeeId);
+			userLocalService.deleteUser(employee.getUserId());
 			employeeLocalService.deleteEmployee(employeeId);
 		} catch (Exception e){
 			log.error(e.getMessage(), e);
@@ -98,7 +151,7 @@ public class EmployeeWebPortlet extends MVCPortlet {
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
 		List<Employee> employeeList = employeeLocalService.getEmployees(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-		renderRequest.setAttribute("employeeList", employeeList);
+		renderRequest.setAttribute(EmployeeWebPortletKeys.EMPLOYEE_LIST, employeeList);
 		super.render(renderRequest, renderResponse);
 	}
 
@@ -107,12 +160,12 @@ public class EmployeeWebPortlet extends MVCPortlet {
 	public void serveResource(ResourceRequest request, ResourceResponse response)
 			throws IOException, PortletException {
 
-		if (!"downloadEmployeePdf".equals(ParamUtil.getString(request, "action"))) {
+		if (!EmployeeWebPortletKeys.DOWNLOAD_EMPLOYEE_PDF.equals(ParamUtil.getString(request, EmployeeWebPortletKeys.ACTION))) {
 			return;
 		}
 
 		try {
-			long employeeId = ParamUtil.getLong(request, "employeeId");
+			long employeeId = ParamUtil.getLong(request, EmployeeWebPortletKeys.EMPLOYEE_ID);
 			Employee employee = employeeLocalService.getEmployee(employeeId);
 
 			if (employee == null) {
@@ -121,11 +174,14 @@ public class EmployeeWebPortlet extends MVCPortlet {
 			}
 
 			String content = "Employee Id : " + employee.getEmployeeId() + "\n" +
-				"First Name : " + employee.getFirstName() + "\n" +
-				"Last Name : " + employee.getLastName() + "\n" +
+				"User Id : " + employee.getUserId() + "\n" +
+				"Name : " + employee.getFirstName() + " " + employee.getLastName() + "\n" +
 				"Email : " + employee.getEmailAddress() + "\n" +
+				"Designation : " + employee.getDesignation() + "\n" +
 				"Phone Number : " + employee.getPhoneNumber() + "\n" +
-				"City : " + employee.getCity();
+				"Address : " + employee.getAddressLine1() + ", " + employee.getAddressLine2() + "\n" +
+				"City : " + employee.getCity() + "\n" +
+				"Post Code / Zip Code : " + employee.getZipCode();
 
 			ByteArrayOutputStream out = new ByteArrayOutputStream();
 
@@ -155,6 +211,8 @@ public class EmployeeWebPortlet extends MVCPortlet {
 		}
 	}
 
+	@Reference
+	UserLocalService userLocalService;
 	@Reference
 	CounterLocalService counterLocalService;
 	@Reference
